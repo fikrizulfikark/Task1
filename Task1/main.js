ca7c43d1708057f66c772c8af6a13dd2607912fb
Fikri Zulfikar Kusmana <81325832+fikrizulfikark@users.noreply.github.com>
2021-04-29 13:06:30 +0700

    Add files via upload

---
+++ Task1/main.js
@@ -0,0 +1,195 @@
+document.getElementById("text1").addEventListener("keyup",function(){
+    var box1=document.getElementsByClassName("red");
+    var box2=document.getElementsByClassName("green");
+    var box3=document.getElementsByClassName("yellow");
+    var box4=document.getElementsByClassName("blue");
+    var box5=document.getElementsByClassName("brown");
+    var box6=document.getElementsByClassName("gray");
+    var box7=document.getElementsByClassName("purple");
+    var box8=document.getElementsByClassName("pink");
+
+    if(document.getElementById("text1").value == "red"){
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "green"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "yellow"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "blue"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "brow"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "gray"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if(document.getElementById("text1").value == "purple"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box8.length; i++) {
+            box8[i].style.display = 'none';
+        }
+    }
+   else if (document.getElementById("text1").value == "pink"){
+        for (var i = 0; i < box1.length; i++) {
+            box1[i].style.display = 'none';
+        }
+        for (var i = 0; i < box2.length; i++) {
+            box2[i].style.display = 'none';
+        }
+        for (var i = 0; i < box3.length; i++) {
+            box3[i].style.display = 'none';
+        }
+        for (var i = 0; i < box4.length; i++) {
+            box4[i].style.display = 'none';
+        }
+        for (var i = 0; i < box5.length; i++) {
+            box5[i].style.display = 'none';
+        }
+        for (var i = 0; i < box6.length; i++) {
+            box6[i].style.display = 'none';
+        }
+        for (var i = 0; i < box7.length; i++) {
+            box7[i].style.display = 'none';
+        }
+    }
+});
